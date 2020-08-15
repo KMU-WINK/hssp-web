2383c14280597d328369ae253ae716ab264d2c0c
wwjdtm <jje0ng@kookmin.ac.kr>
2020-08-15 16:01:57 +0900

    modify signup page #3

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import sReact from "react";
 import Link from 'next/link';
 import {Layout} from "../components/Layout";
 import {InputBox} from "../components/InputBox";
@@ -7,10 +7,12 @@ const SignUp = () => {
 
     return <>
         <Layout>
+            <h1 style={styles.h1}>회원가입</h1>
             <InputBox
-                name="아이디"
-                placeholder="아이디를 입력하세요"
+                name="전화번호"
+                placeholder="전화번호 입력하세요"
                 type="text"
+                detailmessage="고속상황전파체계는 아이디 대신 전화번호로 로그인해요!"
             />
             <InputBox
                 name="패스워드"
@@ -18,13 +20,32 @@ const SignUp = () => {
                 type="password"
             />
             <InputBox
-                name="이메일 "
-                placeholder="이메일 입력하세요"
-                type="email"
+                name="이름"
+                placeholder="이름을 입력하세요"
+                type="text"
+            />
+            <InputBox
+                name="군 분류"
+                placeholder=""
+
+            />
+            <InputBox
+                name="생년월일"
+                placeholder=""
 
             />
         </Layout>
     </>;
 };
+
+const styles = {
+    h1 : {
+        fontSize : 24,
+        marginTop : 90,
+        marginLeft : 20,
+
+    }
+
+}
 
 export default SignUp;
